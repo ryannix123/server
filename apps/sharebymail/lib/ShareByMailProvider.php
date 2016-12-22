df900f56ad911de019061551945962488f4ff443
Roeland Jago Douma <roeland@famdouma.nl>
2016-12-22 21:44:21 +0100

    Offload acceslist creation to providers
    * This allows for effective queries.
    * Introduce currentAccess parameter to speciy if the users needs to have
    currently acces (deleted incomming group share). (For notifications)
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- apps/sharebymail/lib/ShareByMailProvider.php
+++ apps/sharebymail/lib/ShareByMailProvider.php
@@ -854,4 +854,8 @@ class ShareByMailProvider implements IShareProvider {
 		return $shares;
 	}
 
+	public function getAccessList($nodes, $currentAccess) {
+		return ['users' => [], 'remote' => false, 'public' => false];
+	}
+
 }
