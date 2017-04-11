87ba0bf86c1bf50730e845730b6a5382720931c1
Joas Schilling <coding@schilljs.com>
2017-04-11 12:40:36 +0200

    Adjust docs and make !$currentAccess simpler
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/sharebymail/lib/ShareByMailProvider.php
+++ apps/sharebymail/lib/ShareByMailProvider.php
@@ -854,6 +854,9 @@ class ShareByMailProvider implements IShareProvider {
 		return $shares;
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	public function getAccessList($nodes, $currentAccess) {
 		$ids = [];
 		foreach ($nodes as $node) {
