4ce97ccea9d03569317257f79be0aad650602f23
Bjoern Schiessle <bjoern@schiessle.org>
2017-04-11 21:12:09 +0200

    fix mail signatures, signatures always have to start with '-- '
    Signed-off-by: Bjoern Schiessle <bjoern@schiessle.org>

--- apps/sharebymail/templates/altmailpassword.php
+++ apps/sharebymail/templates/altmailpassword.php
@@ -27,6 +27,6 @@ p($l->t("Cheers!"));
 print_unescaped("\n");
 ?>
 
-	--
+<?php print_unescaped("-- \n"); ?>
 <?php p($theme->getName() . ' - ' . $theme->getSlogan()); ?>
 <?php print_unescaped("\n".$theme->getBaseUrl());
